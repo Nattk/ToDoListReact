6aedc2e4fd4c69462efecca4d37456f5f28abe45
Nattan <kifoyinattan@gmail.com>
2019-12-13 10:18:37 +0100

    set up projet

---
+++ src/components/listCard/listCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const listCard = (props) => (
+  <section className="list-card">
+    {props.children}
+  </section>
+)
+
+export default listCard
---
+++ src/containers/TodoList/TodoList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class TodoList extends React.Component {
+    state = {
+      todolists: []
+    }
+
+    render () {
+      return (
+        <main>
+          <article>
+            <h1>Créer une TodoList</h1>
+            <section>
+
+            </section>
+          </article>
+        </main>
+      )
+    }
+}
+
+export default TodoList
